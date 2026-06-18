229b306fd17ebe793481b1c746233bf1d848e07d
Roman Sobolev <rdunstoppable@gmail.com>
2026-06-18 13:11:59 +0300

    add new camera 7_st

---
+++ configs/camera_7_st.py
@@ -0,0 +1,17 @@
+CAMERA_CONFIG = {
+    "camera_id":
+        "ordynka-intellect-7205/DeviceIpint.69/SourceEndpoint.video:0:0",
+
+    "archive":
+        "hosts/ordynka-intellect-7205/MultimediaStorage.AliceBlue/MultimediaStorage",
+
+    "roi":
+        {
+            "x1": 1626,
+            "y1": 333,
+            "x2": 1917,
+            "y2": 1075
+        },
+
+    "detection_mode": "simple"
+}
