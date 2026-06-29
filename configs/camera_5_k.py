731716aeb2647088723e86c4d58ab987bdac79bd
Roman Sobolev <rdunstoppable@gmail.com>
2026-06-29 18:59:11 +0300

    add new camera 5_k

---
+++ configs/camera_5_k.py
@@ -0,0 +1,17 @@
+CAMERA_CONFIG = {
+    "camera_id":
+        "ordynka-intellect-5205/DeviceIpint.23/SourceEndpoint.video:0:0",
+
+    "archive":
+        "hosts/ordynka-intellect-5205/MultimediaStorage.AliceBlue/MultimediaStorage",
+
+    "roi":
+        {
+            "x1": 1838,
+            "y1": 66,
+            "x2": 2588,
+            "y2": 1312
+        },
+
+    "detection_mode": "simple"
+}
